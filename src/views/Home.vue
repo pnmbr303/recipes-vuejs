5e21346a9ace3494c2dc7f6f5ca1bcf9f13c850f
monmima <monmima@protonmail.com>
2021-09-17 16:38:56 -0400

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+
+  <div id="app">
+      <li v-for="item in response" v-bind:key="item.title">
+        <div >{{ item.title }}</div>
+      </li>
+  </div>
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data() {
+      return {
+          response: null
+      }
+  },
+  mounted: function() {
+
+      fetch('https://jsonplaceholder.typicode.com/posts')
+          .then(res => res.json())
+          .then(data => this.response = data) // pass the data to the response variable
+          .then(console.log("test"))
+          .then(console.log("other test"))
+          .then(data => console.log(data)) // print to the console
+      .catch(function (err) {
+          console.log('ERROR CATCHED - ' + err);
+      });
+  }
+}
+</script>
---
+++ src/views/Test.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is an test page</h1>
+    <button class="btn btn-danger">Button</button>
+    <button>Button</button>
+  </div>
+  <p>npm install --save bootstrap</p>
+</template>
